dd2e89c04d390305cc918e2037cea0aea1a32933
aiChenK <aichenk@qq.com>
2022-02-15 17:31:01 +0800

    Site updated: 2022-02-15 17:31:01

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d625d13283f7e3b64e9da7fff54b7ddd";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
